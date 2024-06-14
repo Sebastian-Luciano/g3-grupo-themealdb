81c4b7cb0cfad56550c7c3e8692d8e39a16b9d18
SJLucianoMarceliano <sebastianperu7@gmail.com>
2024-06-14 14:24:10 -0500

    Ruta Creada

--- src/components/MealDetails/MealDetails.jsx
+++ src/components/MealDetails/MealDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function MealDetails() {
+    const {category} = useParams();
     const [meals, setMeals] = useState([]); // Inicializar como un array
 
     async function getData() {
